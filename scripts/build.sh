038d42f36e80cbcae98e9f1ea2c2cfe7a8c3c39a
Bryan Housel <bhousel@gmail.com>
2026-03-17 11:16:18 -0400

    Dereference symbolic links when preparing `/dist` files

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,9 @@
 rm -rf ./dist
 mkdir -p ./dist
 
-# Copy the Rapid dist folders
-cp -R ./node_modules/rapid2/dist ./dist/rapid2
-cp -R ./node_modules/rapid3/dist ./dist/rapid3
+# Copy the Rapid dist folders (use -L to dereference any symlinks)
+cp -RL ./node_modules/rapid2/dist ./dist/rapid2
+cp -RL ./node_modules/rapid3/dist ./dist/rapid3
 
 # Remove example code
 rm -rf ./dist/rapid2/examples
